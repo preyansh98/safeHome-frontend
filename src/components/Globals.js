be41bff5924909c43fe17439a68b60921b0a18e0
preyansh98 <preyansh98@gmail.com>
2020-02-11 11:26:44 -0500

    change backend url

--- src/components/Globals.js
+++ src/components/Globals.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 const Globals = () => {
     //constants
-    global.API_ENDPOINT = "http://192.168.0.126:8080/api/"; 
+    global.API_ENDPOINT = "http://safe-home.herokuapp.com:8080/api/"; 
     global.MAPS_API_KEY = "AIzaSyBBgb9w4vEBeX9zSoLOBC2hB7omCHBi5SU"; 
     global.JSON_SUCCESS = "success";
 
